7f5834a484f1c463df6cea37cc338e7f157bc6e8
Jigesh Mehta <jmehta2@ncsu.edu>
2015-09-22 08:44:53 -0400

    Add search box in index page of book

--- library-app/app/models/book.rb
+++ library-app/app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ActiveRecord::Base
+  def self.search(search)
+     where("title LIKE :search OR ISBN = :search2 OR author LIKE :search OR description LIKE :search OR status = :search2",search2: "#{search}",search: "%#{search}%")
+  end
 end
