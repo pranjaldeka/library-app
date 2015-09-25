1c4ad6bdb4c47404f30c3659bcaf7e7875c19937
Pranjal Deka <pranjal.deka87@gmail.com>
2015-09-24 20:33:10 -0400

    Added password length validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 	format: { with: VALID_EMAIL_REGEX },
 	uniqueness: { case_sensitive: false }
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
+	validates :password, presence: true, length: { minimum: 6, maximum: 25 }, allow_nil: true
 
 	has_many :checkout_histories
 	has_many :books, through: :checkout_histories
